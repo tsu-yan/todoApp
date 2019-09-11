1667d5137b4a2aec346145b27cca2b776fd6a6e1
hirotomo-tsugawa <33314322+hirotomo-tsugawa@users.noreply.github.com>
2019-09-11 11:56:04 +0900

    router追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
